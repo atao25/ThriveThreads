39d29b7f306b73d960446575a9525fa145346409
racheljune2004@gmail.com <racheljune2004@gmail.com>
2024-03-09 23:38:44 -0800

    edited image source

--- src/pages/components/Navbar.js
+++ src/pages/components/Navbar.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import './Navbar.css'
-import logo from '/Users/allisontao/Desktop/cmdf/src/images/catimg.png'
-import profile from '/Users/allisontao/Desktop/cmdf/src/images/profileimg.png'
+import logo from '../../images/catimg.png'
 
 
 const Navbar = () => {
   return (
     <nav>
         <div className = "logo-container">
-            <img src= {logo} alt="Cat Image" style={{ width: '100px', height: '100px' }}></img>
+            <img src={logo} alt="Cat Image" style={{ width: '100px', height: '100px' }}></img>
             <span>ThriveThreads</span>
         </div>
         <div>
